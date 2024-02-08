7ecc576273e63ff630c44f8f443340789e7e98d6
Emmett Deen <emmett.deen@kickasscoding.com>
2024-02-08 14:21:18 -0500

    feat: init project with vgv cli

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.fatl_gemini_chatbot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:fatl_gemini_chatbot/app/app.dart';
+import 'package:fatl_gemini_chatbot/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
